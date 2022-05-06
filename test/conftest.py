0e8871bb30f1e859d36e618feb6e4e6a875bd713
Sam Morgan <sam@lightmatter.com>
2022-05-06 14:23:47 -0700

    Filter request URI data

--- test/conftest.py
+++ test/conftest.py
@@ -7,26 +7,40 @@ from welkin import Client
 
 HEADER_BLACKLIST = [("Authorization", "API_TOKEN")]
 POST_DATA_BLACKLIST = [("secret", "API_TOKEN")]
-# REQUEST_BLACKLIST = ["secret"]
+REQUEST_BLACKLIST = ["secret"]
 RESPONSE_BLACKLIST = ["token"]
+CLIENT_INIT = dict(
+    tenant=os.environ["WELKIN_TENANT"],
+    instance=os.environ["WELKIN_INSTANCE"],
+    api_client=os.environ["WELKIN_API_CLIENT"],
+    secret_key=os.environ["WELKIN_SECRET"],
+)
+
+
+@pytest.fixture(scope="module")
+def client():
+    """Get an authenticated Welkin API client."""
+    return Client(**CLIENT_INIT)
 
 
 @pytest.fixture(scope="module")
 def vcr(vcr):
     vcr.filter_headers = HEADER_BLACKLIST
     vcr.filter_post_data_parameters = POST_DATA_BLACKLIST
-    # vcr.before_record_request = scrub_request(REQUEST_BLACKLIST)
+    vcr.before_record_request = scrub_request(CLIENT_INIT)
     vcr.before_record_response = scrub_response(RESPONSE_BLACKLIST)
     return vcr
 
 
-# def scrub_request(blacklist, replacement="REDACTED"):
-#     def before_record_request(request):
-#         request.body = filter_body(request.body, blacklist, replacement)
+def scrub_request(blacklist, replacement="REDACTED"):
+    def before_record_request(request):
+        # request.body = filter_body(request.body, blacklist, replacement)
+        for k, v in blacklist.items():
+            request.uri = request.uri.replace(v, f"{k}_{replacement}")
 
-#         return request
+        return request
 
-#     return before_record_request
+    return before_record_request
 
 
 def scrub_response(blacklist, replacement="REDACTED"):
@@ -48,14 +62,3 @@ def filter_body(body, blacklist, replacement):
             body_json[k] = replacement
 
     return json.dumps(body_json).encode()
-
-
-@pytest.fixture(scope="module", params=[False])
-def client():
-    """Get an authenticated Welkin API client."""
-    return Client(
-        tenant=os.environ["WELKIN_TENANT"],
-        instance=os.environ["WELKIN_INSTANCE"],
-        api_client=os.environ["WELKIN_API_CLIENT"],
-        secret_key=os.environ["WELKIN_SECRET"],
-    )
