53b78bb20b625f16180e3310eb66c99f2a5c4f6e
Sam Morgan <sam@lightmatter.com>
2022-05-11 11:39:13 -0700

    Add patient create test

--- test/test_patients.py
+++ test/test_patients.py
@@ -4,6 +4,15 @@ from welkin.models.patient import Patient, Patients
 
 
 @pytest.mark.vcr()
+def test_patient_create(client, vcr_cassette):
+    patient = client.Patient(firstName="Foo", lastName="Bar").create()
+
+    assert isinstance(patient, Patient)
+    assert hasattr(patient, "id")
+    assert len(vcr_cassette) == 1
+
+
+@pytest.mark.vcr()
 def test_patient_read(client, vcr_cassette):
     patient_id = "092ae416-1c0d-4e14-be22-9cc8dafacdbd"
     patient = client.Patient(id=patient_id).get()
