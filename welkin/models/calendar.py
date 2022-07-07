c0e3d8e3262b136c989de43da1c9c14edaa2c835
Sam Morgan <sam@lightmatter.com>
2022-07-06 19:09:16 -0700

    Add enum for work hours days of the week

--- welkin/models/calendar.py
+++ welkin/models/calendar.py
@@ -24,6 +24,16 @@ class EventMode(Enum):
     VIDEO = "VIDEO"
 
 
+class Days(Enum):
+    MONDAY = 1
+    TUESDAY = 2
+    WEDNESDAY = 3
+    THURSDAY = 4
+    FRIDAY = 5
+    SATURDAY = 6
+    SUNDAY = 7
+
+
 class CalendarEvent(Resource):
     def create(self):
         # TODO: Accept User and Patient instances as participants
