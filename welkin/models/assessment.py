3181133858cf467e55df908e640156de194e1c9c
Eddie Cohen <eddie@lightmatter.com>
2022-08-04 11:04:01 -0400

    adding assessment records

---
+++ welkin/models/assessment.py
@@ -0,0 +1,40 @@
+from welkin.models.base import Collection, Resource
+
+
+class AssessmentRecord(Resource):
+    def create(self):
+        return super().post(
+            f"{self._client.instance}/patients/{self._parent.id}/assessment-records"
+        )
+
+    def get(self):
+        return super().get(
+            f"{self._client.instance}/patients/{self._parent.id}/assessment-records/{self.id}"
+        )
+
+    def update_status(self, **kwargs):
+        return super().put(
+            f"{self._client.instance}/patients/{self._parent.id}/assessment-records/{self.id}",
+            kwargs,
+        )
+
+    def update_answers(self, **kwargs):
+        print(kwargs)
+        return super().put(
+            f"{self._client.instance}/patients/{self._parent.id}/assessment-records/{self.id}/answers",
+            **kwargs,
+        )
+
+    def delete(self):
+        return super().delete(
+            f"{self._client.instance}/patients/{self._parent.id}/assessment-records/{self.id}",
+        )
+
+
+class AssessmentRecords(Collection):
+    def get(
+        self,
+    ):
+        path = f"{self._client.instance}/patients/{self._parent.id}/assessment-records"
+
+        return super().get(path)
