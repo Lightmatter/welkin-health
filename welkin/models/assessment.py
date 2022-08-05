3500343c05f8b1172058a6032c303d74b6e85688
Eddie Cohen <eddie@lightmatter.com>
2022-08-05 11:07:19 -0400

    adding patient id arg to assessmentrecords

--- welkin/models/assessment.py
+++ welkin/models/assessment.py
@@ -2,38 +2,45 @@ from welkin.models.base import Collection, Resource
 
 
 class AssessmentRecord(Resource):
-    def create(self):
+    def create(self, patient_id: str = None):
         return super().post(
-            f"{self._client.instance}/patients/{self._parent.id}/assessment-records"
+            f"{self._client.instance}/patients/{self.get_patient_id(patient_id)}/assessment-records"
         )
 
-    def get(self):
+    def get(self, patient_id: str = None):
         return super().get(
-            f"{self._client.instance}/patients/{self._parent.id}/assessment-records/{self.id}"
+            f"{self._client.instance}/patients/{self.get_patient_id(patient_id)}/assessment-records/{self.id}"
         )
 
-    def update_status(self, **kwargs):
+    def update_status(self, patient_id: str = None, **kwargs):
         return super().put(
-            f"{self._client.instance}/patients/{self._parent.id}/assessment-records/{self.id}",
+            f"{self._client.instance}/patients/{self.get_patient_id(patient_id)}/assessment-records/{self.id}",
             kwargs,
         )
 
-    def update_answers(self, **kwargs):
+    def update_answers(self, patient_id: str = None, **kwargs):
         return super().put(
-            f"{self._client.instance}/patients/{self._parent.id}/assessment-records/{self.id}/answers",
+            f"{self._client.instance}/patients/{self.get_patient_id(patient_id)}/assessment-records/{self.id}/answers",
             **kwargs,
         )
 
-    def delete(self):
+    def delete(self, patient_id: str = None):
         return super().delete(
-            f"{self._client.instance}/patients/{self._parent.id}/assessment-records/{self.id}",
+            f"{self._client.instance}/patients/{self.get_patient_id(patient_id)}/assessment-records/{self.id}",
         )
+
+    def get_patient_id(self, patient_id):
+        return patient_id if patient_id else self._parent.id
 
 
 class AssessmentRecords(Collection):
-    def get(
-        self,
-    ):
-        path = f"{self._client.instance}/patients/{self._parent.id}/assessment-records"
+    def get(self, patient_id: str = None, **kwargs):
 
-        return super().get(path)
+        if patient_id:
+            patientId = patient_id
+        else:
+            patientId = self._parent.id
+
+        path = f"{self._client.instance}/patients/{patientId}/assessment-records"
+
+        return super().get(path, **kwargs)
