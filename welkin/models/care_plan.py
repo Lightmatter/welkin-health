1bb56f0b9d21babd91db8d6557f9e4eacdd898a3
Alden Turner <alden@lightmatter.com>
2022-08-12 11:24:14 -0400

    Added ability to pass patient id as argument to CarePlan and CarePlanOverview methods. Added utils function to traverse Resource._parent for patient_id

--- welkin/models/care_plan.py
+++ welkin/models/care_plan.py
@@ -1,22 +1,31 @@
 from welkin.models.base import Resource
+from welkin.models.util import find_patient_id_in_parents
 
 
 class CarePlanOverview(Resource):
-    def update(self):
+    def update(self, patient_id: str = None):
+        if not patient_id:
+            # self._parent -> CarePlan
+            # CarePlan._parent -> Patient
+            patient_id = find_patient_id_in_parents(self)
         return super().put(
-            f"{self._client.instance}/patients/{self._parent._parent.id}/care-plan/overview"
+            f"{self._client.instance}/patients/{patient_id}/care-plan/overview"
         )
 
 
 class CarePlan(Resource):
     subresources = [CarePlanOverview]
 
-    def create(self):
+    def create(self, patient_id: str = None):
+        if not patient_id:
+            # self._parent -> Patient
+            patient_id = find_patient_id_in_parents(self)
         return super().post(
-            f"{self._client.instance}/patients/{self._parent.id}/care-plan/overview"
+            f"{self._client.instance}/patients/{patient_id}/care-plan/overview"
         )
 
-    def get(self):
-        return super().get(
-            f"{self._client.instance}/patients/{self._parent.id}/care-plan"
-        )
+    def get(self, patient_id: str = None):
+        if not patient_id:
+            # self._parent -> Patient
+            patient_id = find_patient_id_in_parents(self)
+        return super().get(f"{self._client.instance}/patients/{patient_id}/care-plan")
