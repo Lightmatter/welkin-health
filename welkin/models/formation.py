f367793b28a4536080ad5f5359f323cba1f849f5
Eddie Cohen <eddie@lightmatter.com>
2022-09-14 19:08:29 -0400

    add enum for the data types

--- welkin/models/formation.py
+++ welkin/models/formation.py
@@ -1,11 +1,21 @@
+from enum import Enum
+
 from welkin.models.base import Collection, Resource
 from welkin.pagination import PageableIterator
+
+
+class FormationDataType(Enum):
+    CDTS = "cdts"
+    ASSESSMENTS = "assessments"
+    ENCOUNTERS = "encounters"
+    CARE_PLAN = "goal-templates"
 
 
 class Formations(Collection):
     iterator = PageableIterator
 
-    def get(self, data_type: str, version: str = "current", **kwargs):
+    def get(self, data_type: FormationDataType, version: str = "current", **kwargs):
         return super().get(
-            f"{self._client.instance}/formations/{version}/{data_type}", params=kwargs
+            f"{self._client.instance}/formations/{version}/{data_type.value}",
+            params=kwargs,
         )
