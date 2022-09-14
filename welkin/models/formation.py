e7e9b5a19e213fdd5c40d01692b26c2f6e248050
Eddie Cohen <eddie@lightmatter.com>
2022-09-14 19:03:26 -0400

    adding optional kwargs to formations

--- welkin/models/formation.py
+++ welkin/models/formation.py
@@ -5,5 +5,7 @@ from welkin.pagination import PageableIterator
 class Formations(Collection):
     iterator = PageableIterator
 
-    def get(self, data_type: str, version: str = "current"):
-        return super().get(f"{self._client.instance}/formations/{version}/{data_type}")
+    def get(self, data_type: str, version: str = "current", **kwargs):
+        return super().get(
+            f"{self._client.instance}/formations/{version}/{data_type}", params=kwargs
+        )
