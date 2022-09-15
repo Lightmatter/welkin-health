c95aeaf5e6e24044add91cf895b42c066b355516
Sam Morgan <sam@lightmatter.com>
2022-09-15 09:52:36 -0700

    Move assertions out of try/except block

--- test/test_base.py
+++ test/test_base.py
@@ -7,12 +7,14 @@ from welkin.pagination import PageIterator
 def test_collection_pageable(client):
     for v in vars(client).values():
         try:
-            if issubclass(v, Collection):
-                assert hasattr(v, "get"), f"{v} has no get method"
-                assert issubclass(v.iterator, PageIterator)
-
-                args = inspect.getfullargspec(v.get)
-                assert args.varargs is not None, f"{v}.get must accept variable args"
-                assert args.varkw is not None, f"{v}.get must accept variable kwargs"
+            if not issubclass(v, Collection):
+                continue
         except TypeError:
             continue
+
+        assert hasattr(v, "get"), f"{v} has no get method"
+        assert issubclass(v.iterator, PageIterator)
+
+        args = inspect.getfullargspec(v.get)
+        assert args.varargs is not None, f"{v}.get must accept variable args"
+        assert args.varkw is not None, f"{v}.get must accept variable kwargs"
