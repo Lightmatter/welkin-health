c1f88322e427e350d99d707e1b08ecf6546b897c
Sam Morgan <sam@lightmatter.com>
2022-09-15 09:48:13 -0700

    Test for iterator class in collection

--- test/test_base.py
+++ test/test_base.py
@@ -1,13 +1,15 @@
 import inspect
 
 from welkin.models.base import Collection
+from welkin.pagination import PageIterator
 
 
-def test_collection_get_arguments(client):
+def test_collection_pageable(client):
     for v in vars(client).values():
         try:
             if issubclass(v, Collection):
                 assert hasattr(v, "get"), f"{v} has no get method"
+                assert issubclass(v.iterator, PageIterator)
 
                 args = inspect.getfullargspec(v.get)
                 assert args.varargs is not None, f"{v}.get must accept variable args"
