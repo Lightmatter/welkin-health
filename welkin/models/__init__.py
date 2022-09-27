b3fbe806e9ea6ec52538cb561c59cf323b8c070b
Sam Morgan <sam@lightmatter.com>
2022-09-27 12:06:42 -0700

    Move dynamic instance attributes to explicit

--- welkin/models/__init__.py
+++ welkin/models/__init__.py
@@ -14,8 +14,6 @@ from welkin.models.formation import Formations
 from welkin.models.patient import Patient, Patients
 from welkin.models.user import User, Users
 
-# NOTE: If a class isn't imported here and added to __all__, it will not be callable
-# from a `Client` instance. Also, for legibility, keep this list alphabetical.
 __all__ = [
     "Assessment",
     "AssessmentRecord",
