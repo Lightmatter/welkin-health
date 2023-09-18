3aaf8b747e99151a06c2e30ba8db30b1c147d3bc
Sam Morgan <sama4mail@gmail.com>
2023-09-18 14:45:31 -0700

    Minor version bump

--- welkin/__version__.py
+++ welkin/__version__.py
@@ -4,7 +4,7 @@
 __title__ = "welkin"
 __description__ = "Python Welkin Health API Wrapper."
 __url__ = "https://github.com/Lightmatter/welkin-health"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Sam Morgan"
 __author_email__ = "sama4mail@gmail.com"
 __license__ = "GNU GPL 3.0"
