916aa9c639bf594241b0e2277b6de8444d69c9d0
Sam Morgan <sama4mail@gmail.com>
2024-02-28 14:01:17 -0800

    Update sphinx deps

--- docs/conf.py
+++ docs/conf.py
@@ -1,31 +1,19 @@
-"""Configuration file for the Sphinx documentation builder.
-
-This file only contains a selection of the most common options. For a full
-list see the documentation:
-https://www.sphinx-doc.org/en/master/usage/configuration.html
-"""
-import sys
-from pathlib import Path
-
-root = Path(__file__).parent.parent
-sys.path.insert(0, str(root))
-
-import welkin
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-project = welkin.__title__
-author = welkin.__author__
-copyright = welkin.__copyright__
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-# The full version, including alpha/beta/rc tags
-release = welkin.__version__
-
+project = "foobar"
+copyright = "2024, Sam Morgan"
+author = "Sam Morgan"
+release = "1"
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "autoapi.extension",
     "sphinx.ext.autodoc",
@@ -45,32 +33,12 @@ autoapi_options = [
 ]
 autoapi_type = "python"
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [
-    "_build",
-    "Thumbs.db",
-    ".DS_Store",
-]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "sphinx_rtd_theme"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-
-source_suffix = {
-    ".rst": "restructuredtext",
-    ".md": "markdown",
-}
