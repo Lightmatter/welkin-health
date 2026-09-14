5717064c1645947e43f36fe7779e719f90fe5f43
Eugenia Kim <eugenia.k@joinzest.com>
2026-09-14 18:21:12 -0400

    remove commented out tests

--- test/test_pdt.py
+++ test/test_pdt.py
@@ -23,39 +23,3 @@ def test_pdt_read(client, vcr_cassette):
     assert isinstance(pdt, PDT)
     assert pdt.id == pdt_id
     assert len(vcr_cassette) == 1
-
-
-# @pytest.mark.vcr
-# def test_pdt_read_all(client, vcr_cassette):
-#     pdts = client.PDTs().get(pdt_name="pdt-hospitals", size=1)
-
-#     assert isinstance(pdts, PDTs)
-#     assert isinstance(pdts[0], PDT)
-
-#     if len(pdts) > 20:
-#         assert len(vcr_cassette) > 1, "Pagination was expected"
-#     else:
-#         assert len(vcr_cassette) == 1, "Unexpected pagination"
-
-
-# @pytest.mark.vcr
-# def test_pdt_update(client, vcr_cassette):
-#     pdt = client.PDT(id="092ae416-1c0d-4e14-be22-9cc8dafacdbd").get()
-#     name = pdt.firstName
-
-#     pdt.update(firstName="Baz")
-
-#     assert pdt.firstName != name
-#     assert len(vcr_cassette) == 2
-
-
-# @pytest.mark.vcr
-# def test_pdt_delete(client, vcr_cassette):
-#     pdt = client.PDT(id="092ae416-1c0d-4e14-be22-9cc8dafacdbd")
-
-#     with pytest.raises(WelkinHTTPError) as excinfo:
-#         pdt.delete()
-
-#     assert excinfo.value.response.status_code == 403
-
-#     assert len(vcr_cassette) == 1
