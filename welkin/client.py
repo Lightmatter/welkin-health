e4323f11da2a95feeecccd3a590b7184f6e1e50d
Eugenia Kim <eugenia.k@joinzest.com>
2026-09-14 18:18:57 -0400

    get and create for PDT profile to welkin

--- welkin/client.py
+++ welkin/client.py
@@ -107,6 +107,8 @@ class Client(Session):
     PatientProgram = models.PatientProgram
     PatientPrograms = models.PatientPrograms
     Patients = models.Patients
+    PDT = models.PDT
+    PDTs = models.PDTs
     ProgramPhase = models.ProgramPhase
     ProgramPhases = models.ProgramPhases
     Schedules = models.Schedules
---
+++ welkin/models/pdt.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from welkin.models.base import Collection, Resource
+from welkin.pagination import MetaInfoIterator
+
+
+class PDT(Resource):
+    def create(self, pdt_name: str):
+        return super().post(f"{self._client.instance}/pdts/{pdt_name}")
+
+    def get(self):
+        return super().get(f"{self._client.instance}/pdts/{self.pdtName}/{self.id}")
+
+
+class PDTs(Collection):
+    resource = PDT
+    iterator = MetaInfoIterator
